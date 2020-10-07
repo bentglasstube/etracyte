f1591544c605f6ad900d2d77410ce5d0c1b2e61e
Alan Berndt <alan@eatabrick.org>
2020-10-07 12:08:27 -0700

    Fix main loop for emscripten.

--- main.cc
+++ main.cc
@@ -2,6 +2,14 @@
 
 #include "title_screen.h"
 
+#ifdef __EMSCRIPTEN__
+#include "emscripten.h"
+
+void step(void* game) {
+  static_cast<Game*>(game)->step();
+}
+#endif
+
 int main(int, char**) {
   Game::Config config;
 
@@ -14,7 +22,12 @@ int main(int, char**) {
   Game game(config);
   Screen *start = new TitleScreen();
 
+#ifdef __EMSCRIPTEN__
+  game.start(start);
+  emscripten_set_main_loop_arg(step, &game, 0, true);
+#else
   game.loop(start);
+#endif
 
   return 0;
 }
